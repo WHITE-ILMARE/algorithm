d429c52892b7cbecef05351654d62298a0f8d22f
zhangzheyuan <zhangzheyuan@zuoyebang.com>
2019-12-05 15:50:21 +0800

    这个红绿灯很烦，先记着

--- CCF/201812-2.cpp
+++ CCF/201812-2.cpp
@@ -3,48 +3,22 @@
 
 using namespace std; // 0:µÀÂ·£¬1:ºìµÆ, 2:»ÆµÆ, 3:ÂÌµÆ 
 
-int lights[4];
-
-struct status {
-	int light;
-	int time;
-};
-
-struct status transfer(int light, int time, int accumulate) {
-	time = accumulate - time;
-	if (light == 1) light = 3;
-	else if (light == 2) light = 1;
-	else if (light == 3) light = 2;	
-	while(time >= lights[light]) {
-		time -= lights[light];
-		if (light == 1) light = 3;
-		else if (light == 2) light = 1;
-		else if (light == 3) light = 2;	
-	}
-	time = lights[light] - time;
-	struct status s = {light, time};
-	return s;
-}
-
 int main() {
-	cin >> lights[1] >> lights[2] >> lights[3];
-	int n, result, light, time = 0;
+	ios::sync_with_stdio(false);
+	long long r, y, g, n, result, light, time, sum = 0;
+	cin >> r >> y >> g;
+	sum = r+y+g;
 	cin >> n;
-	for (int i=0;i<n;++i) {
+	while(n--) {
 		cin >> light >> time;
-//		cout << "light=" <<light << " time=" << time << endl;
 		if (light == 0) {
 			result += time;
 		} else {
-			struct status s = transfer(light, time, result);
-//			cout << "---trans---light="<<s.light<<" time="<<s.time<<endl;
-			if (s.light == 1) {
-				result += s.time;
-			} else if (s.light == 2) {
-				result += (lights[1] + s.time);
+			time = ((time-result)%sum+sum)%sum;
+			if (light == 1) {
+				
 			}
 		}
-//		cout << "result = " << result << endl;
 	}
 	cout << result;
 }
