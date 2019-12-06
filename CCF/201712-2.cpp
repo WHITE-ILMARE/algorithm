e54c7507ce550def32645e34b1e2af238bfaafbc
zhangzheyuan <zhangzheyuan@zuoyebang.com>
2019-12-06 18:25:23 +0800

    save

---
+++ CCF/201712-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+const int N = 1000;
+
+
+int main() {
+	// 两种思路：1.模拟循环数数的过程，使用链表；2.得到移除的数学公式，直接计算。用1
+	list<int> l;
+	int n, k;
+	long long count = 1;
+	cin >> n;
+	for (int i=1;i<n+1;++i) l.push_back(i);
+	list<int>::iterator iter = l.begin();
+	while(l.size()>1) {
+		if (count%k==0 || count%10==k) l.remove(iter)
+	} 
+}
