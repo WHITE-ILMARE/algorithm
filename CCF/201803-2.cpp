93be26f70133300cd277372deac59a149969226b
zzy <ilmare@buaa.edu.cn>
2019-12-06 08:40:52 +0800

    balls finsihed, be thoughtful

--- CCF/201803-2.cpp
+++ CCF/201803-2.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 
 struct status {
-	int x, d; // 1:->,0:<-
+	int x, d; // 1:->,-1:<-
 };
 
 int main() {
 	int n,L,t;
 	cin >> n >> L >> t;
-	struct status balls[n] = {struct status(0,1)};
+	struct status balls[n];
 	for (int i=0;i<n;++i) {
 		cin >> balls[i].x;
+		if (balls[i].x == L) balls[i].d = -1;
+		else balls[i].d = 1;
 	}
 	for (int i=0;i<t;++i) { // cal per tick
-		for (int j )
+		for (int j=0;j<n;++j) {
+			balls[j].x += balls[j].d;
+			if (balls[j].x == 0 || balls[j].x == L) balls[j].d = -balls[j].d; 
+		}
+		for (int j=0;j<n;++j) {
+			for(int p=j;p<n;++p) {
+				if (balls[j].x == balls[p].x) {
+					balls[j].d = -balls[j].d;
+					balls[p].d = -balls[p].d;
+				}
+			}
+		
+//			cout<<"("<<balls[j].x<<','<<balls[j].d<<')'<<endl;
+		}
 	}
+	for (int i=0;i<n-1;++i) {
+		cout << balls[i].x << ' ';
+	}
+	cout << balls[n-1].x << endl;
 }
