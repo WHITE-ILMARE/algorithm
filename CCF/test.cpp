59da918afc8d81042e23193653547fd1a2a6f430
zhangzheyuan <zhangzheyuan@zuoyebang.com>
2019-12-25 19:39:12 +0800

    test

--- CCF/test.cpp
+++ CCF/test.cpp
@@ -1,14 +1,39 @@
+
 #include <iostream>
-#include <map>
-
+#include <stack>
+ 
 using namespace std;
-
-int main() {
-	map<int,int>m;
-	m[1]=5;
-	m[3]=2;
-	int n;
-	while(cin >> n) {
-		cout << "m[" << n << "]=" << m[n] << endl;
-	}
-}
+ 
+const int N = 1000;
+int h[N+1];
+ 
+int main()
+{
+    int n, ans=0, area, temp;
+ 
+    // 输入数据
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> h[i];
+    h[n] = 0;
+ 
+    // 计算最大矩形面积
+    stack<int> s;
+    for(int i=0; i<=n; i++) {
+        if (s.empty() || h[s.top()] < h[i])
+            s.push(i);
+        else {
+            temp = s.top();
+            s.pop();            //弹出
+            area = h[temp] * (s.empty() ? i : i - s.top() - 1);
+            if (area > ans)
+                ans = area;
+            --i;
+        }
+    }
+ 
+    // 输出结果
+    cout << ans << endl;
+ 
+    return 0;
+} 
