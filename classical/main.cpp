a422972cd57c579f014764206ff6ac4edcfae767
章哲源 <2480800244@qq.com>
2020-01-15 08:56:03 +0800

    要注意考虑到所有情况，看清输出格式‘

--- classical/main.cpp
+++ classical/main.cpp
@@ -1,51 +1,84 @@
-#include <regex>
-#include <string>
 #include <iostream>
-#include <fstream>
-#include <map>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-string h[100];
-map<string, string> va;
+struct coor {
+    int x, y;
+};
+char c[101][101];
+stack<coor> cs;
 
-int main () {
-	ios::sync_with_stdio(false); 
-//    ifstream ifs("test.txt");
-    int n, v; string name, val;
-//    ifs >> n >> v;
-    cin >> n >> v;
-//	ifs.ignore();
-	cin.ignore();
-//    cout <<"n="<<n<<" v="<<v<<endl;
-    for (int l=0;l<n;++l) {
-//        getline(ifs, h[l]);
-    	getline(cin, h[l]);
-	}
-    for (int l=0;l<v;++l) {
-        string nv;
-//        getline(ifs, nv);
-        getline(cin, nv);
-        int pos = nv.find(" ");
-        int len = nv.length();
-        name = nv.substr(0, pos);
-        val = nv.substr(pos+2, len - pos -3);
-//        cout << name << "=" <<val<<endl;
-        string t = "{{ "+name+" }}";
-        va[t] = val;
+void lineOnce(char c[][101], int x, int y, char ch) {
+    if ((c[x][y] == '-' && ch == '|') || (c[x][y] == '|' && ch=='-') || c[x][y] == '+') c[x][y] = '+';
+    else c[x][y] = ch;
+}
+
+bool isborder(char c[][101], int x, int y, int w, int h, char ch) {
+    return x == -1 || y == -1 || x == w || y == h || c[x][y] == '-' || c[x][y] == '|' || c[x][y] == '+' || c[x][y] == ch;
+}
+
+int main() {
+    for (int k=0;k<101;++k)
+        for (int p=0;p<101;++p)
+            c[k][p] = '.';
+    int n, m, q, op, x1, y1, x2, y2, x, y; char ch;
+    cin >> m >> n >> q;
+    for (int i=0;i<q;++i) {
+//		for (int r=n-1;r>=0;--r) {
+//			for (int v=0;v<m;++v) {
+//				cout << c[v][r] << ' ';
+//			}
+//			cout << endl;
+//		}
+        cin >> op;
+        if (op == 0) {
+            cin >> x1 >> y1 >> x2 >> y2;
+//			cout << "("<<x1<<","<<y1<<"),("<<x2<<","<<y2<<")"<<endl;
+            if (x1 == x2) {
+                if (y1 < y2)
+                    for (int t = y1;t<=y2;++t) lineOnce(c, x1, t, '|');
+                else
+                    for(int t=y2;t<=y1;++t) lineOnce(c, x1, t, '|');
+            } else {
+                if (x1<x2) {
+                    for (int t=x1;t<=x2;++t) lineOnce(c, t, y1, '-');
+                }
+                else
+                    for (int t=x2;t<=x1;++t) lineOnce(c, t, y1, '-');
+            }
+        } else {
+            cin >> x >> y >> ch;
+            coor tc{x, y};
+            cs.push(tc);
+            while(!cs.empty()) {
+                coor tc = cs.top();
+//				cout << "top = ("<<tc.x<<','<<tc.y<<"), size=("<<cs.size()<<")"<<endl;
+                c[tc.x][tc.y] = ch;
+                cs.pop();
+                if (!isborder(c, tc.x-1, tc.y, m, n, ch)) {
+//					cout << "("<<tc.x-1<<","<<tc.y<<") is not border" << endl;
+                    coor temp{tc.x-1, tc.y}; cs.push(temp);
+                }
+                if (!isborder(c, tc.x, tc.y+1, m, n, ch)) {
+//					cout << "("<<tc.x<<","<<tc.y+1<<") is not border" << endl;
+                    coor temp{tc.x, tc.y+1}; cs.push(temp);
+                }
+                if (!isborder(c, tc.x+1, tc.y, m, n, ch)) {
+//					cout << "("<<tc.x+1<<","<<tc.y<<") is not border" << endl;
+                    coor temp{tc.x+1, tc.y}; cs.push(temp);
+                }
+                if (!isborder(c, tc.x, tc.y-1, m, n, ch)) {
+//					cout << "("<<tc.x<<","<<tc.y-1<<") is not border" << endl;
+                    coor temp{tc.x, tc.y-1}; cs.push(temp);
+                }
+            }
+        }
     }
-	for (int l=0;l<n;++l) { // replace per line
-		int start = 0, end = 0;
-		while((start = h[l].find("{{ ", start)) != string::npos) {
-			if ((end = h[l].find(" }}", start+3)) == string::npos) break;
-			string key = h[l].substr(start, end-start+3);
-			h[l].replace(start, end-start+3, va[key]);
-			start += va[key].length();
-			start = h[l].find("{{ ", start); // this is the core to reduce time for finding(90->100)
-		}
-	}
-    for (int i=0;i<n;++i) {
-        cout << h[i] << endl;
+    for (int j=n-1;j>=0;--j) {
+        for (int i=0;i<m;++i) {
+            cout << c[i][j];
+        }
+        cout << endl;
     }
 }
